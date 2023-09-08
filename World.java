245c5c64123ef028f7783eb953700e5cf5dd67f4
Pablo <trmaayt@gmail.com>
2023-09-08 12:49:04 +0200

    v0.0 - wall colisions dont work

--- World.java
+++ World.java
@@ -19,6 +19,12 @@ public class World {
             }
         }
 
+        grid[Sneak.head] = 1;
+
+        for (int i = 0; i < Sneak.points; i++) {
+            grid[Sneak.body[i]] = 1;
+        }
+
         for (int x = 0; x < this.width; x++) {
             grid[x] = 3;
             grid[x + (this.height - 1) * (this.height - 1)] = 3;
@@ -26,12 +32,6 @@ public class World {
         for (int y = 0; y < this.width; y++) {
             grid[y * this.width] = 3;
             grid[(y + 1) * this.width - 1] = 3;
-        }
-
-        grid[Sneak.head] = 1;
-
-        for (int i = 0; i < Sneak.points; i++) {
-            grid[Sneak.body[i]] = 1;
         }
 
         grid[Main.comida.celda] = 2;
